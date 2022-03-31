2ccd9e229c026d4199156a04df4b89934dd84348
i.fucolo <ifucolo>
2022-03-31 17:36:28 +0200

    - migrate from koin to dagger

--- app/src/main/java/com/example/mymoduleexample/MyModuleApplication.kt
+++ app/src/main/java/com/example/mymoduleexample/MyModuleApplication.kt
@@ -2,17 +2,11 @@ package com.example.mymoduleexample
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
 
 @HiltAndroidApp
 class MyModuleApplication: Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        startKoin {
-            androidContext(this@MyModuleApplication)
-        }
     }
 }
--- app/src/main/java/com/example/mymoduleexample/di/AppModule.kt
+++ app/src/main/java/com/example/mymoduleexample/di/AppModule.kt
@@ -12,7 +12,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import org.koin.android.ext.koin.androidContext
 
 @Module
 @InstallIn(SingletonComponent::class)
